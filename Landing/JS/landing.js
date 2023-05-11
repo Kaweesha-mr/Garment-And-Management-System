2481121994b05abe8b7eb346eec805742ecb0f38
kaweesha-mr <kaweesha.mr@gmail.com>
2023-05-11 10:54:11 +0530

    Media icons style and animation added

--- Landing/JS/landing.js
+++ Landing/JS/landing.js
@@ -1,19 +1,7 @@
-//swiper slider
-var swiper = new Swiper(".bg-slider-thumbs", {
-    loop: true,
-    spaceBetween: 10,
-    slidesPerView: 4,
-    freeMode: true,
-    watchSlidesProgress: true,
-  });
-  var swiper2 = new Swiper(".bg-slider", {
-    loop: true,
-    spaceBetween: 10,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    thumbs: {
-      swiper: swiper,
-    },
-  });
+//navigation-bar effecrs on scroll
+
+window.addEventListener("scroll" function(){
+  const header =  document.querySelector("header");
+  header.classList.toggle("sticky",window.scrollY > 0);
+  
+})
