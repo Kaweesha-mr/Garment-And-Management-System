68e0d3052d15b5d5359ac3b9da42bd8eb7a0ced9
kaweesha-mr <kaweesha.mr@gmail.com>
2023-06-11 09:29:07 +0530

    multi login system added for system

--- Landing/login.php
+++ Landing/login.php
@@ -10,28 +10,45 @@ $_SESSION['loggedin'] = false;
 session_start();
 
 
- if($_SERVER['REQUEST_METHOD'] == "POST"){
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
 
     $username = $_POST['username'];
     $password = $_POST['password'];
+
+
+    // todo: multi login system coding
+
+    // get user id for username from database
+    $sql = "SELECT `User_id` FROM `register_user` WHERE `username` = '$username'";
+    $result = mysqli_query($conn, $sql);
+    $result = mysqli_fetch_array($result);
+
+    //check if username 1st 3 letters contain prefix USR
+    $prefix = substr($result[0], 0, 3);
+
+    //prinr prefix using script
+    echo '<script>alert("' . $prefix . '")</script>';
+
+    //if prefix is USR then login as user
+    if ($prefix == "USR") {
 
     //get the password in the database
     $sql = "SELECT `Passwords` FROM `register_user` WHERE `username` = '$username'";
     $result = mysqli_query($conn, $sql);
     $result = mysqli_fetch_array($result);
-    
+
 
     //store result in a variable
     $result = $result[0];
 
     //verify the password
-    $verify = password_verify($password,$result);
+    $verify = password_verify($password, $result);
 
     //script to print veriify
-    echo '<script>alert("'.$verify.'")</script>';
+    echo '<script>alert("' . $verify . '")</script>';
 
     //if password is verifyied then login
-    if($verify == 1){
+    if ($verify == 1) {
         //create a js alert to say successfully registered
         echo '<script>alert("Successfully Logged In")</script>';
 
@@ -52,23 +69,38 @@ session_start();
         $_SESSION['userid'] = $result[0];
 
         //print the userid using script
-        echo '<script>alert("'.$_SESSION['userid'].'")</script>';
+        echo '<script>alert("' . $_SESSION['userid'] . '")</script>';
 
         //redirect to dashboard in emp-dashbord file
         header("location: ../emp-dashbord/dashboard.php");
     }
-    else{
+    else {
         //create a js alert to say error
         echo '<script>alert("Invalid Username or Password")</script>';
     }
- }
+    }//if prefix is ADN then login as admin
+    else if ($prefix == "ADN") {
+
+        // !thilin's login system
+    }
+    else {
+        //create a js alert to say error
+        echo '<script>alert("Invalid Username or Password")</script>';
+    }
+
+
+    //todo multi login system coding ended
+
+
+}
 ?>
 
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    
+
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -78,35 +110,38 @@ session_start();
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <link rel="stylesheet" href="./css/Login.css">
 </head>
+
 <body>
-    
+
 
     <div class="container">
-        <header>Login<h2>Login to explore more</h2></header>
-        
-            <form class="login-form" method=POST>
+        <header>Login<h2>Login to explore more</h2>
+        </header>
 
-                        <div class="input-box">        
-                            <!-- add user icon -->
-                            <i class="fas fa-user"></i>
-                            <label>Username</label>
-                            <input type="text" name="username" placeholder="Enter your username" required>
-                        </div>
-    
-                        <div class="input-box">
-                            <!-- add lock icon -->
-                            <i class="fas fa-lock"></i>
-                            <label>Password</label> 
-                            <input type="password" name="password" placeholder="Enter your password" required>
-                        </div>
-                        
-                        <button>Login</button>
+        <form class="login-form" method=POST>
 
-                        <span>If you Don't have an Account <a href="./Registration.php"> Sign Up</a> </span>
-            </form>
-        </div>
+            <div class="input-box">
+                <!-- add user icon -->
+                <i class="fas fa-user"></i>
+                <label>Username</label>
+                <input type="text" name="username" placeholder="Enter your username" required>
+            </div>
+
+            <div class="input-box">
+                <!-- add lock icon -->
+                <i class="fas fa-lock"></i>
+                <label>Password</label>
+                <input type="password" name="password" placeholder="Enter your password" required>
+            </div>
+
+            <button>Login</button>
+
+            <span>If you Don't have an Account <a href="./Registration.php"> Sign Up</a> </span>
+        </form>
+    </div>
     </div>
 
 
 </body>
+
 </html>
