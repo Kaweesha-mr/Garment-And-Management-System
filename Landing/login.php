f83e9cb8dbbbfd7b182738a599897f221df4686f
Thilina Lakshan <127662816+Th1l1na@users.noreply.github.com>
2023-06-29 18:46:41 +0530

    correct login

--- Landing/login.php
+++ Landing/login.php
@@ -112,7 +112,7 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
         $_SESSION['ADNusername'] = $username;
 
         //get user id from user id
-        $sql = "SELECT ADNname,Email FROM `admin` WHERE `username_ADN` = '$username'";
+        $sql = "SELECT Name,Email FROM `admin` WHERE `username_ADN` = '$username'";
 
         //run the query and print output using script
         $result = mysqli_query($conn, $sql);
